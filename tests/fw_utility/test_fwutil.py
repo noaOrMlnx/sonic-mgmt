da5cdef72a853f743109f176e7ae08f5954a1514
Noa Or <noaor@mellanox.com>
2020-01-30 15:55:31 +0200

    Fix review comments - Magic numbers - Using module facts instead separate function - Remove unnecassary files
    Signed-off-by: Noa Or <noaor@mellanox.com>

--- tests/fw_utility/test_fwutil.py
+++ tests/fw_utility/test_fwutil.py
@@ -1,5 +1,4 @@
 import os
-import pdb
 import re
 import pytest
 from common import reboot
@@ -9,47 +8,15 @@ from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
-LATEST_FW_INSTALLED = True
-PRE_LATEST_FW_INSTALLED = False
 FW_UTIL_DATA = {}
 COMPONENTS = {'BIOS', 'CPLD'}
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-BINARIES_DIR = os.path.join(BASE_DIR, 'binaries')
-TMP_DIR = os.path.basename('tmp')
-MACHINE_CONF = "/host/machine.conf"
-ONIE_PLATFORM_KEY = 'onie_platform'
-ABOOT_PLATFORM_KEY = 'aboot_platform'
-
-
-@pytest.fixture(scope='module')
-def platform_type(duthost):
-    """
-    Fixture to dicsover the platform type
-    :param duthost: DUT
-    """
-    val = None
-    stdout = duthost.command("cat {}".format(MACHINE_CONF))
-    if stdout['rc'] != 0:
-        pytest.fail("Could not read conf file")
-
-    stdout = stdout['stdout']
-    for line in stdout.splitlines():
-        tokens = line.split('=')
-        key = tokens[0].strip()
-        value = tokens[1].strip()
-        if key == ONIE_PLATFORM_KEY or key == ABOOT_PLATFORM_KEY:
-            val = value
-            break
-
-    if val is None:
-        pytest.fail("Could not parse platform type. Aborting!")
-
-    yield val
+SUCCESS_CODE = 0
 
 
 def fw_status(duthost):
     stdout = duthost.command("fwutil show status")
-    if stdout['rc'] != 0:
+    if stdout['rc'] != SUCCESS_CODE:
         err = stdout['stderr']
         pytest.fail("Could not execute command 'fwutil show status'")
         raise err
@@ -83,13 +50,14 @@ def parse_status(fw_status):
     return output_data
 
 
-def components_list(platform_type, fw_data, config_file):
+def components_list(duthost, fw_data, config_file):
 
     # config file contains platform string identifier and components separated by ','.
     # x86_64-mlnx_msn2010-r0: BIOS,CPLD
     conf_path = os.path.join(BASE_DIR, config_file)
     with open(conf_path, "r") as config:
         platforms_dict = yaml.safe_load(config)
+        platform_type = duthost.facts['platform']
         components = platforms_dict[platform_type]
 
     for comp in components.split(","):
@@ -99,7 +67,7 @@ def components_list(platform_type, fw_data, config_file):
     return components.split(",")
 
 
-def test_show_positive(request, duthost, platform_type):
+def test_show_positive(request, duthost):
     """
     Purpose of the test is checking all required components appear in
     'fwutil show status' command output according the given config file.
@@ -112,6 +80,7 @@ def test_show_positive(request, duthost, platform_type):
 
     config_file = request.config.getoption("--config_file")
     try:
-        components_list(platform_type, fw_data, config_file)
+        components_list(duthost, fw_data, config_file)
     except LogAnalyzerError as err:
         raise(err)
+
