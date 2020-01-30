8269e84277cac5ace9ef988920c9494ff2b5cc1e
Noa Or <noaor@mellanox.com>
2020-01-30 17:29:29 +0200

    Fix review comments

--- tests/fw_utility/test_fwutil.py
+++ tests/fw_utility/test_fwutil.py
@@ -9,7 +9,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 FW_UTIL_DATA = {}
-COMPONENTS = {'BIOS', 'CPLD'}
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 SUCCESS_CODE = 0
 
@@ -24,12 +23,12 @@ def fw_status(duthost):
     return stdout['stdout']
 
 
-def parse_status(fw_status):
+def parse_status(fw_status, components_list):
     output_data = {}
     output_lines = fw_status.splitlines()[2:]  # Skip the header lines in output
     for line in output_lines:
         split_line = re.split(r'\s{2,}', line)
-        if split_line[1] not in COMPONENTS:  # There is a value in module column
+        if split_line[1] not in components_list:  # There is a value in module column
             chassis = split_line[0]
             module = split_line[1]
             component = split_line[2]
@@ -47,27 +46,27 @@ def parse_status(fw_status):
             'version': version,
             'desc': desc
         }
+    pdb.set_trace()
     return output_data
 
 
-def components_list(duthost, fw_data, config_file):
+@pytest.fixture(scope='module')
+def components_list(request, duthost):
 
+    config_file = request.config.getoption("--config_file")
     # config file contains platform string identifier and components separated by ','.
     # x86_64-mlnx_msn2010-r0: BIOS,CPLD
+    pdb.set_trace()
     conf_path = os.path.join(BASE_DIR, config_file)
     with open(conf_path, "r") as config:
         platforms_dict = yaml.safe_load(config)
         platform_type = duthost.facts['platform']
         components = platforms_dict[platform_type]
 
-    for comp in components.split(","):
-        if comp not in fw_data:
-            raise "Missing component {}".format(comp)
-
-    return components.split(",")
+    yield components.split(",")
 
 
-def test_show_positive(request, duthost):
+def test_show_positive(request, duthost, components_list):
     """
     Purpose of the test is checking all required components appear in
     'fwutil show status' command output according the given config file.
@@ -76,11 +75,7 @@ def test_show_positive(request, duthost):
     :param parse_status: fixture for parsing the status and place in a data structure.
     """
     status_output = fw_status(duthost)
-    fw_data = parse_status(status_output)
-
-    config_file = request.config.getoption("--config_file")
-    try:
-        components_list(duthost, fw_data, config_file)
-    except LogAnalyzerError as err:
-        raise(err)
-
+    fw_data = parse_status(status_output, components_list)
+    for comp in components_list:
+        if comp not in fw_data:
+            raise "Missing component {}".format(comp)
