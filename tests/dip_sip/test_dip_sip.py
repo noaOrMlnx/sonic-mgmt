ab7e41e325430b242c7c864de0668d8dacfb98cd
Noa Or <noaor@mellanox.com>
2020-03-25 16:40:17 +0200

    Add logging messages

--- tests/dip_sip/test_dip_sip.py
+++ tests/dip_sip/test_dip_sip.py
@@ -2,7 +2,6 @@ import pytest
 import ptf.testutils as testutils
 from ipaddress import ip_address
 import logging
-
 logger = logging.getLogger(__name__)
 
 TOPO_LIST = {'t0', 't1', 't1-lag'}
@@ -32,7 +31,7 @@ def lag_facts(dut):
     dst_lag = mg_facts['minigraph_portchannel_interfaces'][0]['attachto']
     facts['src_port'] = src_lag
     facts['dst_port'] = dst_lag
-
+    logger.info("src_lag is {}, dst_lag is {}".format(src_lag, dst_lag))
     # lldp facts
     lldp_facts = dut.lldp()['ansible_facts']['lldp']
     facts['dst_host_mac'] = lldp_facts[mg_facts['minigraph_portchannels'][dst_lag]['members'][0]]['chassis']['mac']
@@ -60,6 +59,7 @@ def port_facts(dut):
     dst_port = mg_facts['minigraph_interfaces'][0]['attachto']
     facts['src_port'] = src_port
     facts['dst_port'] = dst_port
+    logger.info("src_port is {}, dst_port is {}".format(src_port, dst_port))
     # lldp facts
     lldp_facts = dut.lldp()['ansible_facts']['lldp']
     facts['dst_host_mac'] = lldp_facts[dst_port]['chassis']['mac']
@@ -91,6 +91,7 @@ def gather_facts(testbed_devices, testbed):
         pytest.skip("Unsupported topology")
 
     logger.info("Facts gathered successfully")
+
     yield facts
 
 
@@ -106,6 +107,9 @@ def run_test_ipv6(ptfadapter, gather_facts):
         ipv6_hlim=DEFAULT_HLIM_TTL
 
     )
+    logger.info("\nSend Packet:\neth_dst: {}, eth_src: {}, ipv6 ip: {}".format(
+        gather_facts['src_router_mac'], gather_facts['src_host_mac'], dst_host_ipv6))
+    
     testutils.send(ptfadapter, int(gather_facts['src_port_ids'][0]), pkt)
 
     pkt = testutils.simple_udpv6_packet(
@@ -115,6 +119,9 @@ def run_test_ipv6(ptfadapter, gather_facts):
         ipv6_dst=dst_host_ipv6,
         ipv6_hlim=DEFAULT_HLIM_TTL-1
     )
+    logger.info("\nExpect Packet:\neth_dst: {}, eth_src: {}, ipv6 ip: {}".format(
+        gather_facts['dst_host_mac'], gather_facts['dst_router_mac'], dst_host_ipv6))
+    
     port_list = [int(port) for port in gather_facts['dst_port_ids']]
     testutils.verify_packet_any_port(ptfadapter, pkt, port_list, timeout=WAIT_EXPECTED_PACKET_TIMEOUT)
 
@@ -122,15 +129,16 @@ def run_test_ipv6(ptfadapter, gather_facts):
 def run_test_ipv4(ptfadapter, gather_facts):
     logger.info("Running test with ipv4 packets")
     dst_host_ipv4 = str(ip_address(unicode(gather_facts['dst_router_ipv4'])) + 1)
-
     pkt = testutils.simple_udp_packet(
         eth_dst=gather_facts['src_router_mac'],
         eth_src=gather_facts['src_host_mac'],
         ip_src=dst_host_ipv4,
         ip_dst=dst_host_ipv4,
         ip_ttl=DEFAULT_HLIM_TTL
-
     )
+    logger.info("\nSend Packet:\neth_dst: {}, eth_src: {}, ipv6 ip: {}".format(
+        gather_facts['src_router_mac'], gather_facts['src_host_mac'], dst_host_ipv4))
+    
     testutils.send(ptfadapter, int(gather_facts['src_port_ids'][0]), pkt)
 
     pkt = testutils.simple_udp_packet(
@@ -140,6 +148,9 @@ def run_test_ipv4(ptfadapter, gather_facts):
         ip_dst=dst_host_ipv4,
         ip_ttl=DEFAULT_HLIM_TTL-1
     )
+    logger.info("\nExpect Packet:\neth_dst: {}, eth_src: {}, ipv6 ip: {}".format(
+        gather_facts['dst_host_mac'], gather_facts['dst_router_mac'], dst_host_ipv4))
+    
     port_list = [int(port) for port in gather_facts['dst_port_ids']]
     testutils.verify_packet_any_port(ptfadapter, pkt, port_list, timeout=WAIT_EXPECTED_PACKET_TIMEOUT)
 
